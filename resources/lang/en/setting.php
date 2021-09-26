152fd3049b03315efab8eb2892e6760fc986ee84
Death-satan <2703895209@qq.com>
2021-09-27 07:42:54 +0800

    优化配置项显示

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author    : Death-Satan
+ * @date      : 2021/9/27
+ * @createTime: 7:40
+ * @company   : Death撒旦
+ * @link      https://www.cnblogs.com/death-satan
+ */
+return [
+    'title'=>'友情提示',
+    'content'=>'本插件无需配置项'
+];
---
+++ resources/lang/zh_CN/setting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author    : Death-Satan
+ * @date      : 2021/9/27
+ * @createTime: 7:40
+ * @company   : Death撒旦
+ * @link      https://www.cnblogs.com/death-satan
+ */
+return [
+    'title'=>'Friendly tips',
+    'content'=>'No configuration items are required for this plug-in'
+];
